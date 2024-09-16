f474a991fd8d39994dca378799347a612562ab66
Traver <traver.blumenfeld@mymail.champlain.edu>
2024-09-16 11:06:27 -0400

    Successful refactor! Now onto play

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -6,6 +6,8 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <iostream>
+#include <TraverB/shader.h>
+
 using namespace std;
 
 const int SCREEN_WIDTH = 2080;
@@ -42,66 +44,13 @@ int main() {
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glNamedBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	const char* vertexShaderSource = R"(#version 330 core
-		layout(location = 0) in vec3 aPos;
-		layout (location = 1) in vec4 aColor;
-		out vec4 Color;
-		uniform float _Time;
-		void main()
-		{
-		   Color=aColor;
-			vec3 pos= aPos;
-			pos.y+=(cos(_Time*1.5+(pos.x*0.5)+(pos.z*0.75))*0.5);
-		   gl_Position = vec4(aPos.x, pos.y, aPos.z, 1.0);
-})";
 	//position
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	//color
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3*sizeof(float)));
 	glEnableVertexAttribArray(1);
-	unsigned int vertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	int  success{};
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	char infoLog[512];
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << endl;
-	}
-	const char* fragmentShaderSource = R"(#version 330 core
-out vec4 FragColor;
-in vec4 Color;
-uniform float _Time;
-void main()
-{
-    FragColor = Color * abs(tan(_Time));
-})";
-	unsigned int fragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		cout << "Error:Shader::Fragment:Compilation failed" << infoLog << endl;
-	}
-	//created and compiled fragment shader by here
-
-	unsigned int shaderProgram;
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
-		cout << "Error:shaderProgram::Compilation failed" << infoLog << endl;
-	}
+	Shader mine("assets/Shader.vert", "assets/Shader.frag");
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents(); 
@@ -110,18 +59,16 @@ void main()
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
+		mine.use();
 
-		int timeLoc = glGetUniformLocation(shaderProgram, "_Time");
-		glUniform1f(timeLoc, time);
+		mine.setFloat("_Time", time);
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		//Drawing happens here!
 		glfwSwapBuffers(window);
 	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-	glDeleteProgram(shaderProgram);
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	printf("Shutting down...");
 }
