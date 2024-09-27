74a3421bc1f65925e87424a7caf42ac131a889a7
Traver <traver.blumenfeld@mymail.champlain.edu>
2024-09-26 23:13:53 -0400

    Got the crystal loaded, now I just have to keep moving

--- assignments/Assignment2_Indicies/assets/Shader.frag
+++ assignments/Assignment2_Indicies/assets/Shader.frag
@@ -6,6 +6,5 @@ uniform float _Time;
 uniform sampler2D ourTexture;
 void main()
 {
-    FragColor =texture(ourTexture, TexCoord);
-    //* vec4(Color * abs(tan(_Time)), 1.0)
+    FragColor =texture(ourTexture, TexCoord)* vec4(Color*abs(tan(_Time)));
 }
--- assignments/Assignment2_Indicies/main.cpp
+++ assignments/Assignment2_Indicies/main.cpp
@@ -82,10 +82,8 @@ int main() {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	// load image, create texture and generate mipmaps
 	int width, height, nrChannels;
-	const char* path = new char;
-	path = "textures/stainedGlass.png";
 	// The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-	unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
+	unsigned char* data = stbi_load("C:/Users/Traver/Desktop/GMD-202/gpr200-fa2024-template-Traver/assignments/Assignment2_Indicies/textures/27239.png", &width, &height, &nrChannels, 0);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
